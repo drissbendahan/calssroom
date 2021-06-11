d0e2dda53c718a420f5d74fbd0a4677c58639cb2
drissbendahan <drissbenedahane@gmail.com>
2021-06-11 03:14:13 +0100

    two

--- CoursSite/settings.py
+++ CoursSite/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yluuw2+zgn++xgn%y!di8cg8q4_-ruovv-nyd0i$tlm&)vo*sg'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
